141eb0eccaa81ffa96c652af45842cd782ed2604
“fredrickUdom” <fredrickudom@gmail.com>
2023-11-26 15:32:37 +0100

    setting our port in env format

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PROJECT_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, ()=>console.log(`project running on port: ${port}`));
 }
 bootstrap();
